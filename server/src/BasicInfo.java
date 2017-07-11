458e0946d566456434f05f5fae38837f90877ef3
jorinak <jorinak@princeton.edu>
2017-07-11 14:57:24 -0400

    Create BasicInfo.java

---
+++ server/src/BasicInfo.java
@@ -0,0 +1,63 @@
+package com.tigerconnect.model;
+
+import java.io.Serializable;
+
+public class BasicInfo implements Serializable{
+	
+	private static final long serialVersionUID = 1238918602068356152L;
+	
+	String fname;
+	String lname;
+	String cyear;
+	String blurb;
+	String major;
+	String rcollege;
+	
+	public BasicInfo(String firstname, String lastname, String classyear, String description, String major, String rescollege) {
+		super();
+		this.fname    = firstname;
+		this.lname    = lastname;
+		this.cyear    = classyear;
+		this.blurb    = description;
+		this.major    = major;
+		this.rcollege = rescollege;
+	}
+	
+	public String getFirstname() {
+		return fname;
+	}
+	public void setFirstname(String fname) {
+		this.fname = fname;
+	}
+	public String getLastname() {
+		return lname;
+	}
+	public void setLastname(String lname) {
+		this.lname = lname;
+	}
+	public String getClassyear() {
+		return cyear;
+	}
+	public void setClassyear(String cyear) {
+		this.cyear = cyear;
+	}
+	public String getBlurb() {
+		return blurb;
+	}
+	public void setBlurb(String blurb) {
+		this.blurb = blurb;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String getRcollege() {
+		return rcollege;
+	}
+	public void setRcollege(String rcollege) {
+		this.rcollege = rcollege;
+	}
+	
+}
