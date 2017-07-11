7d8a662b432506a3faa6cc412f47c2bf1f066b28
jorinak <jorinak@princeton.edu>
2017-07-11 00:33:44 -0400

    Create Test1.java

---
+++ server/Test1.java
@@ -0,0 +1,39 @@
+package com.tigerconnect.model;
+
+import java.io.Serializable;
+
+public class Test1 implements Serializable{
+	
+	private static final long serialVersionUID = 1238918602068356152L;
+	
+	String username;
+	String password;
+	String email;
+	
+	
+	public Test1(String username, String password, String email) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.email    = email;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
