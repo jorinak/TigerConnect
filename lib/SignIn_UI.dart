d5840ebde36e620c7e20dc063c6bb2071450458e
jorinak <jorinak@princeton.edu>
2017-07-17 16:21:59 -0400

    Update SignIn_UI.dart

--- lib/SignIn_UI.dart
+++ lib/SignIn_UI.dart
@@ -7,17 +7,16 @@ class SignIn extends StatefulWidget {
 }
 
 class SignInState extends State<SignIn> {
+
   String passwordFieldContent;
-
   String password = "";
-
   String username = "";
 
-  // helper method that makes a get request
+  // helper method that makes a get request to verify user account
   checkData() async {
     var httpClient = createHttpClient();
     var response = await httpClient.get(
-        'http://localhost:8080/tiger-connect/signin?login=${username}&password=${password}');
+        'http://localhost:8080/tiger-connect/signin?username=${username}&password=${password}');
     String result = response.body;
     if (result == 'true\n') {
       Navigator.of(context).pushNamed("/HomeProfilePage");
