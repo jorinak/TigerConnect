d5e1c5f342fb405ed5325f1ea746d7651296a0d6
jorinak <jorinak@princeton.edu>
2017-07-17 11:31:38 -0400

    Create SignUp_UI.dart

---
+++ lib/SignUp_UI.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SignUp extends StatefulWidget {
+
+  @override
+  SignUpState createState() => new SignUpState();
+}
+
+
+class SignUpState extends State<SignUp> {
+
+  String email = "";
+  String newUsername = "";
+  String newPassword = "";
+  String passwordFieldContent;
+
+  // helper method that makes a get request
+  inputData() async {
+    var httpClient = createHttpClient();
+    var response = await httpClient.get(
+        'http://localhost:8080/tiger-connect/signup?username=${newUsername}&password=${newPassword}&email=${email}');
+    Navigator.of(context).pushNamed("/ProfileInfo");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        child: new Center(
+            child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new TextField(
+                    decoration: new InputDecoration(
+                        hintText: "Princeton Email",
+                        labelText: "Princeton Email"),
+                    onChanged: (String _email) {
+                      setState(() {
+                        email = _email;
+                      });
+                    },
+                  ),
+
+                  new TextField(
+                    decoration: new InputDecoration(
+                        hintText: "Create Your Username",
+                        labelText: "Create Your Username"),
+                    onChanged: (String _newUSr) {
+                      setState(() {
+                        newUsername = _newUSr;
+                      });
+                    },
+                  ),
+
+                  new TextField(
+                    decoration: new InputDecoration(
+                      hintText: "Create Your Password",
+                      labelText: "Create Your Password",
+                    ),
+
+                    obscureText: true,
+
+                    onChanged: (String _newPass) {
+                      setState(() {
+                        newPassword = _newPass;
+                      });
+                      passwordFieldContent = _newPass;
+                    },
+                  ),
+
+                  new IconButton(
+                    icon: new Icon(
+                        Icons.blur_circular,
+                        color: Colors.orange),
+                    iconSize: 70.0,
+                    onPressed: () {
+                      inputData();
+                    },
+                  ),
+
+                  new Text("Sign Up"),
+
+                ]
+            )
+        )
+    );
+  }
+}
