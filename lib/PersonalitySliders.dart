655e1f069f89cc9cf043e6342a506ff845410351
jorinak <jorinak@princeton.edu>
2017-07-18 17:33:21 -0400

    Update PersonalitySliders.dart

--- lib/PersonalitySliders.dart
+++ lib/PersonalitySliders.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'globals.dart' as globals;
 import 'package:tiger_connect/CupertinoWidgets.dart';
 
 class PersonalitySliders extends StatefulWidget {
@@ -20,7 +22,21 @@ class _PersonalitySlidersState extends State<PersonalitySliders> {
   double _extraversion = 0.0;
   double _agreeable = 0.0;
   double _stability = 0.0;
-
+  
+  // helper method that makes a get request to add personality preferences
+  inputData() async {
+    int o = _openness.toInt();
+    int c = _conscientiousness.toInt();
+    int e = _extraversion.toInt();
+    int a = _agreeable.toInt();
+    int n = _stability.toInt();
+    var httpClient = createHttpClient();
+    var response = await httpClient.get(
+        'http://localhost:8080/tiger-connect/personality?id=${globals.user_id}&scale1=${o}&scale2=${c}&scale3=${e}&scale4=${a}&scale5=${n}');
+    print(response.statusCode);
+    print(response.body);
+    Navigator.of(context).pushNamed("/Categories");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +51,9 @@ class _PersonalitySlidersState extends State<PersonalitySliders> {
                 fontSize: 18.0),
           ),
           color: Colors.black,
-          onPressed: () {Navigator.of(context).pushNamed("/Categories");},
+          onPressed: () {
+            inputData();
+          },
         )
     );
 
