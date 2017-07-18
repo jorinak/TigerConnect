c9c8f268e1c8fef7338bfbd47847eff00c37f6fc
jorinak <jorinak@princeton.edu>
2017-07-18 17:32:10 -0400

    Update BasicInfo.dart

--- lib/BasicInfo.dart
+++ lib/BasicInfo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'globals.dart' as globals;
 
 
 class BasicInfo extends StatefulWidget {
@@ -22,7 +23,16 @@ class BasicInfoState extends State<BasicInfo> {
   String major = "";
 
   String shortDescription = "";
-
+  
+  // helper method that makes a get request to add user preferences
+  inputData() async {
+    var httpClient = createHttpClient();
+    var response = await httpClient.get(
+        'http://localhost:8080/tiger-connect/basicinfo?id=${globals.user_id}&fname=${firstName}&lname=${lastName}&cyear=${classYear}&blurb=${shortDescription}&major=${major}&rcollege=${resCollege}');
+    print(response.statusCode);
+    print(response.body);
+    Navigator.of(context).pushNamed("/Personality");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +47,9 @@ class BasicInfoState extends State<BasicInfo> {
                 fontSize: 18.0),
           ),
             color: Colors.black,
-            onPressed: () {Navigator.of(context).pushNamed("/Personality");},
+            onPressed: () {
+              inputData();
+            },
         )
     );
 
