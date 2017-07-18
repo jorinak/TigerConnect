f6ef58ec730bab1d3cd825c53e04c10220b0faf0
jorinak <jorinak@princeton.edu>
2017-07-18 12:23:51 -0400

    Update SignIn_UI.dart

--- lib/SignIn_UI.dart
+++ lib/SignIn_UI.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'globals.dart' as globals;
 
 class SignIn extends StatefulWidget {
   @override
@@ -17,11 +18,13 @@ class SignInState extends State<SignIn> {
     var httpClient = createHttpClient();
     var response = await httpClient.get(
         'http://localhost:8080/tiger-connect/signin?username=${username}&password=${password}');
-    String result = response.body;
-    if (result == 'true\n') {
-      Navigator.of(context).pushNamed("/HomeProfilePage");
-    } else {
+    String reply = response.body;
+    if (reply == 'false\n') {
       // Display the text "Invalid credentials".
+    } else {
+      globals.user_id = int.parse(reply);
+      print(globals.user_id);
+      Navigator.of(context).pushNamed("/HomeProfilePage");
     }
   }
 
