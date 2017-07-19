bf0a7dc1671131e478f2c9a906751ef01e9d1614
jorinak <jorinak@princeton.edu>
2017-07-19 11:11:45 -0400

    Update globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,3 +1,45 @@
 library my_prj.globals;
 
 int user_id = -1;
+
+String u1_firstname = "";
+String u2_firstname = "";
+String u3_firstname = "";
+String u4_firstname = "";
+String u5_firstname = "";
+
+String u1_lastname = "";
+String u2_lastname = "";
+String u3_lastname = "";
+String u4_lastname = "";
+String u5_lastname = "";
+
+String u1_classyear = "";
+String u2_classyear = "";
+String u3_classyear = "";
+String u4_classyear = "";
+String u5_classyear = "";
+
+String u1_description = "";
+String u2_description = "";
+String u3_description = "";
+String u4_description = "";
+String u5_description = "";
+
+String u1_major = "";
+String u2_major = "";
+String u3_major = "";
+String u4_major = "";
+String u5_major = "";
+
+String u1_rescollege = "";
+String u2_rescollege = "";
+String u3_rescollege = "";
+String u4_rescollege = "";
+String u5_rescollege = "";
+
+String u1_interests = "";
+String u2_interests = "";
+String u3_interests = "";
+String u4_interests = "";
+String u5_interests = "";
